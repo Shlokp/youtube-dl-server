0394bf239e6823e724550fcb803b1a15f7d00fce
Shlokp <35976544+Shlokp@users.noreply.github.com>
2020-02-06 15:24:44 +0000

    change how storage directory is handled

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -10,6 +10,8 @@ import youtube_dl
 from pathlib import Path
 from collections import ChainMap
 
+VIDEO_FOLDER = 'video'
+
 app = Bottle()
 
 
@@ -18,7 +20,7 @@ app_defaults = {
     'YDL_EXTRACT_AUDIO_FORMAT': None,
     'YDL_EXTRACT_AUDIO_QUALITY': '192',
     'YDL_RECODE_VIDEO_FORMAT': None,
-    'YDL_OUTPUT_TEMPLATE': f'./static/%(title)s [%(id)s].%(ext)s',
+     'YDL_OUTPUT_TEMPLATE': f'./{VIDEO_FOLDER}/%(title)s [%(id)s].%(ext)s',
     'YDL_ARCHIVE_FILE': None,
     'YDL_SERVER_HOST': '0.0.0.0',
     'YDL_SERVER_PORT': 8080,
@@ -37,7 +39,7 @@ def redirectToCorrectIndexURL():
 
 @app.route('/youtube-dl/static/:filename#.*#')
 def server_static(filename):
-    return static_file(filename, root='./static')
+    return static_file(filename, root=f'./{VIDEO_FOLDER}')
 
 
 @app.route('/video/:filename#.*#')
