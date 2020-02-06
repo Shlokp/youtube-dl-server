14cc0b06ec4a42380472bef5e27dd54c552932e5
Shlokp <35976544+Shlokp@users.noreply.github.com>
2020-02-06 14:20:22 +0000

    write subtitles by default

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -22,6 +22,7 @@ app_defaults = {
     'YDL_ARCHIVE_FILE': None,
     'YDL_SERVER_HOST': '0.0.0.0',
     'YDL_SERVER_PORT': 8080,
+    'YDL_WRITE_SUBTITLES': True,
 }
 
 
@@ -123,6 +124,7 @@ def get_ydl_options(request_options):
         })
 
     return {
+        'writesubtitles': ydl_vars['YDL_WRITE_SUBTITLES'],
         'format': ydl_vars['YDL_FORMAT'],
         'postprocessors': postprocessors,
         'outtmpl': ydl_vars['YDL_OUTPUT_TEMPLATE'],
